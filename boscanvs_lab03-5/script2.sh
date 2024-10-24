ae73fa1b000e04d8c576a94efa9626aa490edf5d
sebastianboscan <sebastianboscan@hotmail.fr>
2024-10-24 14:47:37 -0400

    Lab03-5 completed!

---
+++ boscanvs_lab03-5/script2.sh
@@ -0,0 +1,15 @@
+whoami
+date
+uptime
+if [ -e testfile.txt ]; then
+	echo "File exists!"
+else
+	echo "File not found"
+fi
+touch info.txt
+if [ -e info.txt ]; then
+	echo "File exists!"
+else
+	echo "File not found"
+fi
+rm info.txt
